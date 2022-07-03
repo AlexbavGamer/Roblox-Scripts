b1ce6d88941618f4a06a0ebaa22d5e182403563a
Alexsandre <alexsandrebr@hotmail.com.br>
2022-07-03 02:10:15 -0300

    Update TweenCFrame.lua

--- Functions/TweenCFrame.lua
+++ Functions/TweenCFrame.lua
@@ -1,10 +1,11 @@
 local Promise = loadstring(game:HttpGet("https://raw.githubusercontent.com/AlexbavGamer/Roblox-Scripts/main/Promise.lua"))()
 local TableUtil = loadstring(game:HttpGet("https://raw.githubusercontent.com/AlexbavGamer/Roblox-Scripts/main/TableUtil.lua"))()
-local tInfo = TweenInfo.new();
-
 local TweenService = game:GetService("TweenService")
 
-local TweenModel = function(ModelToTween : Model, CFrame : CFrame)
+local TweenModel = function(ModelToTween : Model, CFrame : CFrame, Time: number, Delay: number)
+	Time = Time or 1000
+	Delay = Delay or 1000
+	local tInfo = TweenInfo.new(Time, Enum.EasingStyle.Quad, Enum.EasingDirection.Out, 0, false, Delay);
     return Promise.new(function(resolve, reject)
         local CFrameValue = Instance.new("CFrameValue")
         CFrameValue.Value = ModelToTween:GetPrimaryPartCFrame()
